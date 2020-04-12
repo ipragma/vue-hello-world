3afd4e7df3f45767cbf6ddd82599ec044c520cd6
iPragma <ipragmafrance@gmail.com>
2020-04-12 09:02:32 +0200

    convert to widget

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
+// module.exports = {
+//   "transpileDependencies": [
+//     "vuetify"
+//   ]
+// }
+
+const webpack = require('webpack')
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      })
+    ]
+  },
+  chainWebpack:
+    config => {
+      config.optimization.delete('splitChunks')
+    }
 }
