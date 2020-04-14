55351b2b007577911c39cd83de0924d08466f11d
iPragma <ipragmafrance@gmail.com>
2020-04-14 09:51:58 +0200

    convert to iframe

--- src/main-web-component-wrapper.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import wrap from '@vue/web-component-wrapper';
-import HelloWorld from './components/HelloWorld';
-
-const CustomElement = wrap(Vue, HelloWorld);
-
-window.customElements.define('my-custom-element', CustomElement);
